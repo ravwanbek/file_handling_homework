a040b680627fd8d8ffd0f29fadbbc7379905ed6a
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-23 00:47:26 +0900

    6

--- file06.py
+++ file06.py
@@ -7,4 +7,13 @@ def main(data:str):
         list: return answer
     """
     
+    list1=[]
+    row=data.split('\n')
+    for i in row:
+        list1.append(len(i))
+    
+
+    return list1
 # Read data from file
+f=open('txt_file\data06.txt').read()
+print(main(f))
